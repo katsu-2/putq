3632b27ea7009395c90218c0635c663ff61d0ca7
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-13 18:22:29 +0900

    feat: editの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content, :answer)
