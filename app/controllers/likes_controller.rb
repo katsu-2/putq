144bc9ad6f929266d9259c245c19439efd643125
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-14 16:43:24 +0900

    feat: indexにハート追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to post_path(@post)
+    redirect_back(fallback_location: post_path(@post))
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_to post_path(@post)
+    redirect_back(fallback_location: post_path(@post))
   end
 
   private
