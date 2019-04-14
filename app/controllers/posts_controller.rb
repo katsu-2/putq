5927469f17135d70e2b0cc44c6bf31ed59d86815
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-14 17:22:18 +0900

    feat: n+1問題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.order('created_at desc').includes(:user)
+    @posts = Post.order('created_at desc').includes(user: :likes)
   end
 
   def show
