9876963d0c0ecff05cd136e45a7687db64e46166
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-20 11:46:27 +0900

    feat: commentコントローラの各アクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
-  def create
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to @post
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    redirect_to @post if @comment.destroy
+  end
 
+  private
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 end
