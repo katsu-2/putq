74bbbf40632697dd7d6598a02a6722eae3d1e48f
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-20 14:14:32 +0900

    feat: コメントmodelのテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    it "コメントがあったら有効" do
+      expect(build(:comment)).to be_valid
+    end
+
+    it "コメントがなかったら無効" do
+      comment = build(:comment, comment: nil)
+      comment.valid?
+      expect(comment.errors[:comment]).to include("が入力されていません。")
+    end
+
+    it "コメントが2001字以上は無効" do
+      comment = build(:comment, comment: "a" * 2001)
+      comment.valid?
+      expect(comment.errors[:comment]).to include("は2000文字以下に設定して下さい。")
+    end
+  end
 end
