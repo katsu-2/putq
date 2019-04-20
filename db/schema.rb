f90ae234d89584cc59ea4fc864d72c5443d3e2cb
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-20 15:43:36 +0900

    feat: post_tagテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_063920) do
+ActiveRecord::Schema.define(version: 2019_04_20_064213) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
