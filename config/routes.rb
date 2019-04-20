f735c04b1b0734f02d50be46da8b8e87b5e99774
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-20 11:36:35 +0900

    feat: コメントのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :posts do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 end
