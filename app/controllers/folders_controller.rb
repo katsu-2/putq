23caad128333ce55f19ffe4c96846a3aa01d66e0
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-22 18:32:16 +0900

    feat: folderにuser_id追加

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,31 @@
 class FoldersController < ApplicationController
+  before_action :get_post, only: [:new, :create]
+
+  def index
+    @folders = Folder.all
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name, {post_ids: []})
+  end
+
+  def get_post
+    @posts = Post.all
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_081817) do
+ActiveRecord::Schema.define(version: 2019_04_22_093126) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_04_22_081817) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -88,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_081817) do
   add_foreign_key "comments", "users"
   add_foreign_key "folder_posts", "folders"
   add_foreign_key "folder_posts", "posts"
+  add_foreign_key "folders", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "post_tags", "posts"
