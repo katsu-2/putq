19e9765998162dba654e46928e3e240dbb736c24
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-22 17:30:54 +0900

    feat: folderコントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :users, only: [:index, :show]
+  resources :folders do
+    resources :posts
+  end
+
   resources :posts do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+
 end
