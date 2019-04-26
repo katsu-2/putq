b2bb529a84472859309fce58098fc10305bdf91c
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-26 19:01:53 +0900

    style: ユーザー詳細ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = current_user.posts.recent
+    @posts = current_user.posts.recent.page(params[:page]).per(6)
   end
 end
