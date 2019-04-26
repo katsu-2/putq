baeb1cb0e7ee4e0c95aa4d38704f4d7488588d5c
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-26 18:41:45 +0900

    style: kaminariのデザイン変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user! , only: [:show, :new,:create, :edit, :update ,:destroy]
 
   def index
-    @posts = Post.recent.includes(user: :likes)
+    @posts = Post.recent.page(params[:page]).per(10).includes(user: :likes)
   end
 
   def show
