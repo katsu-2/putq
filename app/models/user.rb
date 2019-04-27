5aaac4cb28af071c19a7b7d387dacaf8e18028aa
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-27 19:00:03 +0900

    fix: postを削除できないバグを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
-  has_many :likes
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :folders
 
   #validation
