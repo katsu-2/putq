34a309734acea3644026247248e5cb9a34078737
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-28 16:41:15 +0900

    feat: アソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
