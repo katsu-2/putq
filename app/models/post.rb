ca01a595f2f798a6bb1ed10f90da7e0e57f1b0d8
大平勝也 <k8t2y9.cat@gmail.com>
2019-04-28 15:29:30 +0900

    fix: いらないファイルの削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
-  has_many :post_tags, dependent: :destroy
-  has_many :tags, through: :post_tags
   has_many :folder_posts, dependent: :destroy
   has_many :folders, through: :folder_posts
 
