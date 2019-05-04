65a9b47600bdebe0c40d1b5811922fd673a0b9e7
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-04 16:31:57 +0900

    fix: 子カテゴリーコントローラ削除

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :categories, only: [:index, :show] do
-    resources :child_categories, only: [:index, :show]
-  end
+  resources :categories, only: [:index, :show]
 
 end
