98e3ceb25ca1a2299d30a6e7cf0dbb0e3f98fee0
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-05 18:02:52 +0900

    feat: カテゴリー一覧から該当のpostを閲覧できる

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,8 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @child_categories = Category.where(parent_id: @category)
+    # @child_categories = Category.where(parent_id: @category)
+    @posts = Post.where(category_id: @category)
     # binding.pry
   end
 end
