5e72eb7050fd89726df7e434378b54baf04e75e1
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-06 17:32:51 +0900

    fix: バグ解消

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :posts
   belongs_to :parent, class_name: :Category, optional: true
   has_many :children, class_name: :Category, foreign_key: :parent_id
+
+  validates :name, presence: true
 end
